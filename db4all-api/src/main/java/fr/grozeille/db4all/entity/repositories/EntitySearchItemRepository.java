b5239ea4c700ebdbf7f698598bd3d2fddbd697a2
Mathias Kluba <mathias.kluba@gmail.com>
2017-10-14 00:39:47 +0200

    search

--- db4all-api/src/main/java/fr/grozeille/db4all/entity/repositories/EntitySearchItemRepository.java
+++ db4all-api/src/main/java/fr/grozeille/db4all/entity/repositories/EntitySearchItemRepository.java
@@ -1,15 +1,15 @@
 package fr.grozeille.db4all.entity.repositories;
 
-import fr.grozeille.db4all.configurations.stereotype.SolrRepository;
 import fr.grozeille.db4all.entity.model.EntitySearchItem;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.solr.repository.Query;
 import org.springframework.data.solr.repository.SolrCrudRepository;
 
-@SolrRepository
-public interface EntityRepository extends SolrCrudRepository<EntitySearchItem, String> {
+@NoRepositoryBean
+public interface EntitySearchItemRepository extends SolrCrudRepository<EntitySearchItem, String> {
 
-    @Query(value = "text:?0 AND project:?1")
-    Page<EntitySearchItem> findAllAndProject(Pageable pageable, String filter, String project);
+    @Query(value = "text:'?0' AND projectName:'?1'")
+    Page<EntitySearchItem> findAllAndProject(Pageable pageable, String filter, String projectName);
 }
