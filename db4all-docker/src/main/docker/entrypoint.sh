283597e45b08865a76e067667c04e401ad719860
Mathias Kluba <mathias.kluba@gmail.com>
2017-12-22 18:03:45 +0100

    use a single docker image for each process (split in 3 images: hbase, solr & db4all)

--- db4all-docker/src/main/docker/entrypoint.sh
+++ db4all-docker/src/main/docker/entrypoint.sh
@@ -6,44 +6,30 @@ set -euo pipefail
 srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export JAVA_HOME="${JAVA_HOME:-/usr}"
+export SOLR_URL=http://solrcloud:8983/solr
 
-# tries to run zookeepers.sh distributed via SSH, run zookeeper manually instead now
-#RUN sed -i 's/# export HBASE_MANAGES_ZK=true/export HBASE_MANAGES_ZK=true/' /hbase/conf/hbase-env.sh
-echo
-echo "Starting local Zookeeper"
-mkdir -p /hbase/logs/
-/hbase/bin/hbase zookeeper &>/hbase/logs/zookeeper.log &
-echo
+# wait for solr
+until $(curl --output /dev/null --silent --head --fail $SOLR_URL); do
+  printf '.'
+  sleep 5
+done
 
-echo "Starting HBase"
-/hbase/bin/start-hbase.sh
-echo
-
-echo "Starting Solr"
-
-export SOLR_USER="solr"
-
-if [ "$(whoami)" = "root" ]; then
-    su - "$SOLR_USER" <<-EOF
-        # preserve PATH from root
-        export PATH="$PATH"
-        /solr-start.sh
-EOF
-else
-    solr-start.sh
-fi
-
-sleep 180
 
 echo "Creating Project collection"
-solr zk upconfig -z localhost:9983 -n project -d /solr-conf/project
-curl "http://localhost:8983/solr/admin/collections?action=CREATE&name=project&numShards=1&replicationFactor=1&maxShardsPerNode=-1&collection.configName=project"
-curl "http://localhost:8983/solr/admin/collections?action=RELOAD&name=project"
+# solr zk upconfig -z localhost:9983 -n project -d /solr-conf/project
+(cd /solr-conf/project/conf && zip -r - *) > project.zip
+curl -X POST --header "Content-Type:application/octet-stream" --data-binary @project.zip "$SOLR_URL/admin/configs?action=UPLOAD&name=project"
 
-echo "Creating Project entity"
-solr zk upconfig -z localhost:9983 -n entity -d /solr-conf/entity
-curl "http://localhost:8983/solr/admin/collections?action=CREATE&name=entity&numShards=1&replicationFactor=1&maxShardsPerNode=-1&collection.configName=entity"
-curl "http://localhost:8983/solr/admin/collections?action=RELOAD&name=entity"
+curl "$SOLR_URL/admin/collections?action=CREATE&name=project&numShards=1&replicationFactor=1&maxShardsPerNode=-1&collection.configName=project"
+curl "$SOLR_URL/admin/collections?action=RELOAD&name=project"
+
+echo "Creating Entity collection"
+# solr zk upconfig -z localhost:9983 -n entity -d /solr-conf/entity
+(cd /solr-conf/entity/conf && zip -r - *) > entity.zip
+curl -X POST --header "Content-Type:application/octet-stream" --data-binary @entity.zip "$SOLR_URL/admin/configs?action=UPLOAD&name=entity"
+
+curl "$SOLR_URL/admin/collections?action=CREATE&name=entity&numShards=1&replicationFactor=1&maxShardsPerNode=-1&collection.configName=entity"
+curl "$SOLR_URL/admin/collections?action=RELOAD&name=entity"
 
 
 echo "Starting DB4ALL"
